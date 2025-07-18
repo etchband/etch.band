dfeea7a8d6aa1037d592fc02611d6ed2d2ba59e8
Lance Pollard <lp@elk.fm>
2025-07-18 02:53:08 -0700

    save

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Page from '~/lib/frontend/components/pages/Page'
 import { buildMetadata } from '@cluesurf/leaf/utility/metadata'
+import { CURRENT_URL } from '~/lib/shared/utilities/url'
 
 export const metadata = buildMetadata({
   site: 'züs',
   title: 'züs',
   // description: '',
+  images: {
+    url: `${CURRENT_URL}/logo.proto.png`,
+    width: 1590,
+    height: 938,
+    type: 'image/png',
+  },
 })
 
 export default function View() {
